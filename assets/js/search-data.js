f03f69d39b4791497fd1eb2eb601e06bdb974389
vasanthsarathy <vsarathy@gmail.com>
2025-04-16 20:35:04 +0000

    Deploying to gh-pages from @ collective-intelligence-lab/collective-intelligence-lab.github.io@6df98fbc93b30f638c5eafe5c46610f38fc24b0c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/join/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
